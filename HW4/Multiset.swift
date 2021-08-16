9421cce7d069d9a1d6a87d924e892844654ff1fc
alenasidorova <alena5740@mail.ru>
2021-08-16 23:30:26 +0300

    correction multiset

--- HW4/Multiset.swift
+++ HW4/Multiset.swift
@@ -9,37 +9,51 @@ import Foundation
 
 struct Multiset<Element: Hashable> {
     var box : [Element : Int] = [:]
-
+    
     func count(for element: Element) -> Int {
-        for key in box.keys {
-            if key == element, let count = box[element] {
-                return count
-            }
-        }
-        return 0
+        let count = box[element] ?? 0
+        return count
     }
-
+    
     mutating func add(element: Element) {
         if box.isEmpty {
             box[element] = 1
         } else {
-            for key in box.keys {
-                var count = box[element] ?? 0
-                if key == element {
-                    count += 1
-                    box[element] = count
-                } else if count == 0 {
-                    box[element] = 1
-                }
+            var count = box[element] ?? 0
+            if count == 0 {
+                box[element] = 1
+            } else {
+                count += 1
+                box[element] = count
             }
         }
     }
-
+    
+    public var allElements: [Element] {
+       var multiset = [Element]()
+       for (key, value) in box {
+         for _ in 0 ..< value {
+            multiset.append(key)
+         }
+       }
+       return multiset
+     }
+    
     func totalCount() -> Int {
-        return box.count
+        return allElements.count
     }
-
-    mutating func remove(element:Element) {
+    
+    // С методом removeAll я не очень поняла задание, должны ли удаляться все элементы или просто понижаться счетчик элемента. Поэтому сделала два варианта
+    
+    // Вариант 1: удаление элемента полностью
+    mutating func removeAll(element: Element) {
         box.removeValue(forKey: element)
+    }
+    
+    //Вариант 2: уменьшение счетчика элемента
+    mutating func remove(element: Element) {
+        if let count = box[element] {
+            box[element] = count - 1
+        }
     }
 }
